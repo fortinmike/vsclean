396cedd537b3d989e02ae048e149b8f66df539fe
Michaël Fortin <fortinmike@irradiated.net>
2015-04-09 20:41:32 -0400

    Implemented cleaning!

--- lib/clean.rb
+++ lib/clean.rb
@@ -14,7 +14,24 @@ module BinClean
     
     def run
       directories = Dir.glob("**/{bin,obj}").select { |f| File.directory?(f) }
-      puts directories.inspect
+      
+      if directories.none?
+        Console.log_step("All is well with the world... nothing to clean!")
+        return
+      end
+        
+      Console.log_step("Cleaning...") 
+      
+      directories.each { |d| delete(d) }
+      
+      Console.log_step("Done!")
+    end
+    
+    def delete(path)
+      FileUtils.rm_r(path)
+      Console.log_substep("Deleted ./#{path}")
+    rescue StandardError => e
+      Console.log_error("Could not delete './#{path}': #{e.message}")
     end
   end
 end
