b3730f62735d3f2c166ac41205478202a46d0d71
Michaël Fortin <fortinmike@irradiated.net>
2015-04-09 20:41:26 -0400

    Simpler logging logic.

--- lib/console.rb
+++ lib/console.rb
@@ -4,45 +4,27 @@ module BinClean
   class Console
     def self.log_step(message)
       print_arrow
-      puts $stdout.isatty ? message.bold.green : message
+      puts message.bold.green + "\r"
     end
     
     def self.log_substep(message)
       print_arrow
-      puts $stdout.isatty ? message.blue : message
+      puts message.blue + "\r"
     end
     
     def self.log_info(message)
       print_arrow
-      puts $stdout.isatty ? message.white : message
+      puts message.white + "\r"
     end
     
     def self.log_warning(message)
       print_arrow
-      puts $stdout.isatty ? message.yellow : message
+      puts message.yellow + "\r"
     end
     
     def self.log_error(message)
       print_arrow
-      puts $stdout.isatty ? message.red : message
-    end
-    
-    def self.ask_yes_no(message)
-      answered = false
-      while !answered
-        print_arrow
-        print message
-        print " (y/n) "
-        
-        case $stdin.gets.strip.downcase
-        when "y", "yes"
-          answered = true
-          return true
-        when "n", "no"
-          answered = true
-          return false
-        end
-      end
+      puts message.red + "\r"
     end
     
     def self.print_arrow
