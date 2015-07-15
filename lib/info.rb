0176d876648d08bd8542cf3665f5e715adaede96
Michaël Fortin <michael@simbioz.com>
2015-07-15 13:54:33 -0400

    Bumped to 1.0.3.

--- lib/info.rb
+++ lib/info.rb
@@ -1,5 +1,5 @@
 module BinClean
   NAME = "binclean"
-  VERSION = "1.0.2"
+  VERSION = "1.0.3"
   DESCRIPTION = %q{Recursively delete temporary Visual Studio output files from the current directory}
 end
