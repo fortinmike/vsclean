a4dded8e2e9ad7f0a6059af69dc8dd5e040e42de
Michaël Fortin <michael@simbioz.com>
2016-04-25 12:47:21 -0400

    Bump version to 1.0.8

--- lib/info.rb
+++ lib/info.rb
@@ -1,5 +1,5 @@
 module VsClean
   NAME = "vsclean"
-  VERSION = "1.0.7"
+  VERSION = "1.0.8"
   DESCRIPTION = %q{A recursive delete of temporary Visual Studio and ReSharper output files, vsclean performs.}
 end
